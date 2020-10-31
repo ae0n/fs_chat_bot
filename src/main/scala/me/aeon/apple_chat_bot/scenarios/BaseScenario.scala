d31d6adf195ac77bfc2b49b8abbdcd358ea4dd3f
Dmitry Svetlakov <mail@ae0n.name>
2020-10-31 13:40:14 +0100

    updated UserJoinScenario and added BaseScenario

---
+++ src/main/scala/me/aeon/apple_chat_bot/scenarios/BaseScenario.scala
@@ -0,0 +1,17 @@
+package me.aeon.apple_chat_bot.scenarios
+
+import canoe.api.models.Keyboard
+import canoe.models.{InlineKeyboardButton, InlineKeyboardMarkup}
+import canoe.models.outgoing.TextContent
+
+trait BaseScenario {
+
+  def text(str: String): TextContent = TextContent(str)
+
+  def button(text: String, callback: String) = {
+    val keyboardButton = InlineKeyboardButton.callbackData(text, callback)
+    Keyboard.Inline(InlineKeyboardMarkup.singleButton(keyboardButton))
+  }
+
+
+}
--- src/main/scala/me/aeon/apple_chat_bot/scenarios/UserJoinScenario.scala
+++ src/main/scala/me/aeon/apple_chat_bot/scenarios/UserJoinScenario.scala
@@ -1,30 +1,25 @@
 package me.aeon.apple_chat_bot.scenarios
 
 import canoe.api._
-import canoe.api.models.Keyboard
+import canoe.models.User
 import canoe.models.messages.{ChatMemberAdded, TelegramMessage, TextMessage}
-import canoe.models.outgoing.TextContent
-import canoe.models.{InlineKeyboardButton, InlineKeyboardMarkup, User}
+import cats.data.OptionT
 import cats.effect.Async
 import cats.implicits._
 import me.aeon.apple_chat_bot.models.{ChatUser, UserState}
 import me.aeon.apple_chat_bot.services.UserService
 
-class UserJoinScenario[F[_]: TelegramClient: Async](userService: UserService[F]) {
+class UserJoinScenario[F[_]: TelegramClient: Async](userService: UserService[F]) extends BaseScenario {
 
   private val memberAddedPF: PartialFunction[TelegramMessage, ChatMemberAdded] = {
     case m: ChatMemberAdded => m
   }
-
-  def text(str: String): TextContent = TextContent(str)
 
   def sendGreetingMessage(msg: ChatMemberAdded): F[Unit] = {
     msg.newChatMembers.toList.traverse(greetUser(msg, _)).void
   }
 
   def greetUser(msg: ChatMemberAdded, user: User): F[TextMessage] = {
-    val keyboardButton = InlineKeyboardButton.callbackData(strings.imNotABot, "")
-    val markup = InlineKeyboardMarkup.singleButton(keyboardButton)
 
     userService
       .getUserById(user.id)
@@ -36,8 +31,10 @@ class UserJoinScenario[F[_]: TelegramClient: Async](userService: UserService[F])
           msg.chat.send(text(strings.welcomeBackMessage), msg.messageId.some)
         case None =>
           val newUser = ChatUser.fromUser(user).copy(state = UserState.Unchecked)
-          userService.addUser(newUser) >>
-            msg.chat.send(text(strings.userJoinMessage), msg.messageId.some, Keyboard.Inline(markup))
+          for {
+            _ <- OptionT(userService.addUser(newUser)).getOrElseF(Async[F].raiseError(new Throwable("unable to create user")))
+            message <- msg.chat.send(text(strings.userJoinMessage), msg.messageId.some, button(strings.imNotABot, user.id.toString))
+          } yield message
       }
   }
 
