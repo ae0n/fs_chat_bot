8c460c8a07b3c857edfd4c92a2cb0509c3145a6e
Dmitry Svetlakov <mail@ae0n.name>
2020-11-08 01:17:19 +0100

    refactored usage of transactor

--- src/main/scala/me/aeon/apple_chat_bot/Main.scala
+++ src/main/scala/me/aeon/apple_chat_bot/Main.scala
@@ -1,7 +1,8 @@
 package me.aeon.apple_chat_bot
 
 import canoe.api.{Bot, TelegramClient}
-import cats.effect.{Blocker, ExitCode, IO, IOApp}
+import cats.effect.{Blocker, ExitCode, IO, IOApp, Resource}
+import doobie.util.transactor.Transactor
 import fs2._
 import me.aeon.apple_chat_bot.models.AppConfig
 import me.aeon.apple_chat_bot.scenarios.UserJoinScenario
@@ -15,18 +16,26 @@ object Main extends IOApp {
 
   val blocker = Blocker.liftExecutionContext(ExecutionContext.global)
 
+  def startBot(config: AppConfig, transactorResource: Resource[IO, Transactor[IO]]) = {
+      for{
+        transactor <- Stream.resource(transactorResource)
+        implicit0(client: TelegramClient[IO]) <- Stream.resource(
+          TelegramClient.global[IO](config.botToken)
+        )
+        userService <- Stream.eval(UserService[IO](transactor))
+        userJoinScenario <- Stream.eval(UserJoinScenario[IO](userService))
+        _ <- Bot.polling[IO].follow(userJoinScenario.scenario)
+      } yield ()
+    
+  }
+  
   override def run(args: List[String]): IO[ExitCode] = {
 
     val appStream: Stream[IO, ExitCode] = for {
       config <- Stream.eval[IO, AppConfig](ConfigSource.default.loadF[IO, AppConfig](blocker))
       _ = println(config)
-      implicit0(client: TelegramClient[IO]) <- Stream.resource(
-                                                 TelegramClient.global[IO](config.botToken)
-                                               )
-      transactor <- Stream.eval(Database.transactor[IO](config.database))
-      userService <- Stream.eval(UserService[IO](transactor))
-      userJoinScenario <- Stream.eval(UserJoinScenario[IO](userService))
-      _ <- Bot.polling[IO].follow(userJoinScenario.scenario)
+      transactorResource <- Stream.eval(Database.transactor[IO](config.database))
+      _ <- startBot(config, transactorResource)
     } yield ExitCode.Success
     appStream.compile.lastOrError
   }
--- src/main/scala/me/aeon/apple_chat_bot/services/UserService.scala
+++ src/main/scala/me/aeon/apple_chat_bot/services/UserService.scala
@@ -5,31 +5,29 @@ import cats.implicits._
 import doobie._
 import doobie.implicits._
 import me.aeon.apple_chat_bot.dao.ChatUsersDao
-import me.aeon.apple_chat_bot.models.ChatUser
+import me.aeon.apple_chat_bot.models.{ChatUser, UserState}
 
-class UserService[F[_]: Sync](transactorResource: Resource[F, Transactor[F]]) {
+class UserService[F[_]: Sync](transactor: Transactor[F]) {
 
   def getUserById(id: Int): F[Option[ChatUser]] = {
-    transactorResource.use { xa =>
-      ChatUsersDao.findById(id).transact(xa)
-    }
+    ChatUsersDao.findById(id).transact(transactor)
   }
 
   def addUser(user: ChatUser) = {
-    transactorResource.use { xa =>
-      ChatUsersDao.insert(user).transact(xa)
-    }
+    ChatUsersDao.insert(user).transact(transactor)
   }
 
   def getOrCreateUser(user: ChatUser) = {
     getUserById(user.id).flatMap(_.fold(addUser(user))(_.some.pure[F]))
   }
 
+  def updateUserState(userId: Int, state: UserState) = {}
+
 }
 
 object UserService {
 
-  def apply[F[_]: Sync](transactorResource: Resource[F, Transactor[F]]) = {
-    (new UserService[F](transactorResource)).pure[F]
+  def apply[F[_]: Sync](transactor: Transactor[F]) = {
+    (new UserService[F](transactor)).pure[F]
   }
 }
